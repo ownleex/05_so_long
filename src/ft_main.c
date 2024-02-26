d341a2d8e891ce490d6de5d2689cc2e0079ee3a0
ownleex <ownleex@gmail.com>
2024-02-26 15:54:42 +0100

    change

--- src/ft_main.c
+++ src/ft_main.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 14:00:38 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/02/26 01:32:27 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/02/26 15:54:10 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,28 @@ int	main(int argc, char **argv)
 			ft_printf("\nError\nProblème d'allocation mémoire pour game.\n");
 			return (1);
 		}
+		// Initialisation des variables de game ici
+		game->cnt_items = 0;
+		game->visited_items = 0;
+		game->path_found = 0;
+		game->visited = NULL;
+		// Continuez avec les autres initialisations nécessaires
 		load_check_init(game, argv[1]);
 	}
 	return (1);
 }
+
+/*
+game = malloc(sizeof(t_vars));
+if (!game)
+{
+    ft_printf("\nError\nProblème d'allocation mémoire pour game.\n");
+    return (1);
+}
+// Initialisation des variables de game ici
+game->cnt_items = 0;
+game->visited_items = 0;
+game->path_found = 0;
+game->visited = NULL;
+// Continuez avec les autres initialisations nécessaires
+*/
